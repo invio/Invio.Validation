2c344303df4ded2bfcf167623ad0abbfac5a084a
Saqib Rokadia <saqib@invioinc.com>
2017-07-19 13:13:24 -0700

    Switching to netcoreapp1.1 in build script.

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ dotnet restore
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net46 so restrict for now
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet test ./test/Invio.Validation.Tests -c Release -f netcoreapp1.0
+dotnet test ./test/Invio.Validation.Tests -c Release -f netcoreapp1.1
 
 # Instead, run directly with mono for the full .net version
 dotnet build ./test/Invio.Validation.Tests -c Release -f net46
