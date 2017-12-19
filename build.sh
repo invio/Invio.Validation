0dedcce13dcf22dcbb1a724fd6fcc85ef0cdf7d5
Brian Caruso <carusobp@gmail.com>
2017-12-18 17:20:44 -0800

    Update dependencies, use .NET Core v2 for tests

--- build.sh
+++ build.sh
@@ -11,9 +11,6 @@ fi
 
 dotnet restore
 
-# Ideally we would use the 'dotnet test' command to test netcoreapp and net46 so restrict for now
-# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
-
-dotnet test ./test/Invio.Validation.Tests/Invio.Validation.Tests.csproj -c Release -f netcoreapp1.1
+dotnet test ./test/Invio.Validation.Tests/Invio.Validation.Tests.csproj -c Release
 
 dotnet pack ./src/Invio.Validation -c Release -o ../../artifacts
